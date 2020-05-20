c7d7dfcc55b886784c51e8d70ad27e2f1accbee2
Lucas Albano <lucas@albano.us>
2020-05-20 19:38:12 -0400

    fixed business name

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,27 +1,40 @@
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
 
-const HomeBanner = () => (
-  <section id="banner" className="major">
-    <div className="inner">
-      <header className="major">
-        <h1>Fulcrum Design and Marketing</h1>
-      </header>
-      <div className="content">
-        <p>
-          We Design
-          <br />
-          With You in Mind
-        </p>
-        <ul className="actions">
-          <li>
-            <a href="#one" className="button next scrolly">
-              Get Started
-            </a>
-          </li>
-        </ul>
+const HomeBanner = () => {
+
+  const data = useStaticQuery(graphql`
+    query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+  `)
+  return (
+    <section id="banner" className="major">
+      <div className="inner">
+        <header className="major">
+          <h1>{data.site.siteMetadata.title}</h1>
+        </header>
+        <div className="content">
+          <p>
+            We Design
+            <br/>
+            With You in Mind
+          </p>
+          <ul className="actions">
+            <li>
+              <a href="#one" className="button next scrolly">
+                Get Started
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default HomeBanner
